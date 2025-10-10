c74466fd9c40ef5060f4cf662689fbdeb78ef97b
DongHunKim <ddong3525@gmail.com>
2025-10-10 09:30:25 +0900

    Initialize Spring AI Project

---
+++ src/main/java/com/ampersandor/sat_rag/SatRagApplication.java
@@ -0,0 +1,13 @@
+package com.ampersandor.sat_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ampersandor/sat_rag/SatRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampersandor.sat_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
